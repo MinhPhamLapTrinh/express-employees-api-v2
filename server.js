466a68371af73639fba49fad8e24dafc567f2370
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-04-15 15:06:50 -0400

    update

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ passport.use(strategy);
 // add passport as application-level middleware
 app.use(passport.initialize());
 
-const port = 4000; // Port number  to run the server
+const port = process.env.PORT || 4000; // Port number  to run the server
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
