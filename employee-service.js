627b3182deb8f69a68531d28e7a30b86378eb764
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-04-17 13:36:14 -0400

    fix working hours by date

--- employee-service.js
+++ employee-service.js
@@ -235,10 +235,10 @@ export function employeeClockOut(id) {
 export function getAllEmployeeByDate(startDate, endDate) {
   return new Promise(function (resolve, reject) {
     const start = new Date(startDate);
-    start.setDate(start.getDate());
+    start.setDate(start.getDate() - 1);
     start.setHours(0, 0, 0, 0);
     const end = new Date(endDate);
-    end.setDate(end.getDate());
+    end.setDate(end.getDate() - 1);
     end.setHours(23, 59, 59, 999);
     Employee.find({})
       .then((employees) => {
