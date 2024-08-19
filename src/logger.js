24f912f108c2b4a4e269153e7f6de9d7d9bbc0b4
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-08-18 21:22:30 -0400

    Add pino logger

---
+++ src/logger.js
@@ -0,0 +1,18 @@
+// src/logger.js
+
+import pino from "pino";
+
+const options = { level: process.env.LOG_LEVEL || "info" };
+
+// If we're doing `debug` logging, make the logs easier to read
+if (options.level === "debug") {
+  options.transport = {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  };
+}
+
+// Create and export a Pino Logger instance:
+export default pino(options);
